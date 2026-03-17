fb81eb279f9138f4f62dded0cfd74f9a1bbba257
slskdn CI <slskdn@proton.me>
2026-03-17 19:45:29 +0000

    chore(homebrew): update formula to 0.24.5-slskdn.58 [skip ci]

--- Formula/slskdn.rb
+++ Formula/slskdn.rb
@@ -2,20 +2,20 @@ class Slskdn < Formula
   desc "Batteries-included Soulseek web client"
   homepage "https://github.com/snapetech/slskdn"
   license "AGPL-3.0-or-later"
-  version "0.24.5-slskdn.57"
+  version "0.24.5-slskdn.58"
   on_macos do
     on_arm do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.57/slskdn-main-osx-arm64.zip"
-      sha256 "10ecb29eda4a8f69ae06b83aff6f8b7ab7aff8c19859616254434840caa6c0c9"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.58/slskdn-main-osx-arm64.zip"
+      sha256 "f055d9647abcae9587a47397b7c4b257904df0f34918b71dd11ae6b406d36a6f"
     end
     on_intel do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.57/slskdn-main-osx-x64.zip"
-      sha256 "0727765782c1b07a97ad2e9435f423686027b7f60b984b427a19c1b047322e0a"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.58/slskdn-main-osx-x64.zip"
+      sha256 "bafd740f3f5401f50e01068f358c0b592e06c616518dcb948919792fc4daa032"
     end
   end
   on_linux do
-    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.57/slskdn-main-linux-x64.zip"
-    sha256 "57d205e559665d5dc8e0af24a11159fa72401d6fa4ebb882a9a902e009d06733"
+    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.58/slskdn-main-linux-x64.zip"
+    sha256 "279ba3af54db2e97972352a5238750d9960f1ea420f8be2f81f920fc9271d62e"
   end
   def install
     libexec.install Dir["*"]
