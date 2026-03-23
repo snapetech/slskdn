10a5d947ca7150b5fed309a48e7d12fa347c2359
Keith <keith@slskdn.dev>
2026-03-23 12:59:25 -0600

    fix(core): skip timeout timers for indefinite waits

--- tests/slskd.Tests.Unit/Core/WaiterTests.cs
+++ tests/slskd.Tests.Unit/Core/WaiterTests.cs
@@ -1,6 +1,7 @@
 namespace slskd.Tests.Unit.Core;
 
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -40,5 +41,29 @@ public class WaiterTests
         await Assert.ThrowsAsync<TaskCanceledException>(() => thirdWait);
         Assert.False(waiter.IsWaitingFor(firstKey));
         Assert.False(waiter.IsWaitingFor(secondKey));
+    }
+
+    [Fact]
+    public void WaitIndefinitely_DoesNotAllocateTimeoutTokenSource()
+    {
+        using var waiter = new Waiter();
+        var key = new WaitKey("message", 3);
+
+        _ = waiter.WaitIndefinitely(key);
+
+        var waits = waiter.GetType().GetProperty("Waits", BindingFlags.Instance | BindingFlags.NonPublic)
+            ?? throw new InvalidOperationException("Waits property was not found.");
+        var waitDictionary = (System.Collections.IDictionary)waits.GetValue(waiter)!;
+        var queue = waitDictionary[key]!;
+        var tryPeek = queue.GetType().GetMethod("TryPeek")
+            ?? throw new InvalidOperationException("TryPeek method was not found.");
+        var parameters = new object?[] { null };
+        Assert.True((bool)tryPeek.Invoke(queue, parameters)!);
+
+        var pendingWait = parameters[0]!;
+        var timeoutTokenSourceProperty = pendingWait.GetType().GetProperty("TimeoutTokenSource", BindingFlags.Instance | BindingFlags.NonPublic)
+            ?? throw new InvalidOperationException("TimeoutTokenSource property was not found.");
+
+        Assert.Null(timeoutTokenSourceProperty.GetValue(pendingWait));
     }
 }
